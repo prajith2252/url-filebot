8ca4a560a704539e8091f45556d4830018670a29
Jack <92579700+JohnWickKeanue@users.noreply.github.com>
2022-12-25 14:33:08 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,1 @@
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/GreyMatterbots/url-auto-delete-shortener-bot /Eva
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Eva
-fi
-cd /Eva
-pip3 install -U -r requirements.txt
-echo "Starting Bot...."
-python3 bot.py
+python3 update.py && python3 bot.py
