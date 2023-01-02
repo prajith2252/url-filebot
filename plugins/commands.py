ea61ce33cc9b74a2aeba371d7e799ad1415782d7
prajith2252 <83197685+prajith2252@users.noreply.github.com>
2023-01-03 01:23:08 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -54,11 +54,17 @@ async def start(client, message):
             InlineKeyboardButton('♻️ ΛBOUT ♻️', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
+
         await message.reply_photo(
+
             photo=random.choice(PICS),
+
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+
             reply_markup=reply_markup,
+
             parse_mode=enums.ParseMode.HTML
+
         )
         return
     if AUTH_CHANNEL and not await is_subscribed(client, message):
