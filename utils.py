94fe8fc94043316b6ad65f67a4e45f59ce68d4fa
prajith2252 <83197685+prajith2252@users.noreply.github.com>
2023-01-03 09:02:45 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -386,7 +386,7 @@ async def get_shortlink(link):
     if "http" == https:
         https = "https"
         link = link.replace("http", https)
-    url = f'https://Clicksfly.com/api'
+    url = f'https://gtlink.co/api'
     params = {'api': URL_SHORTNER_WEBSITE_API,
               'url': link,
               }
