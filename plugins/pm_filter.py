cd4f7334776f0eb9cf5489090ed9c49ced9db478
prajith2252 <83197685+prajith2252@users.noreply.github.com>
2023-02-14 13:10:56 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -20,6 +20,9 @@ from database.filters_mdb import (
     find_filter,
     get_filters,
 )
+import os
+req_channel = int(os.environ.get('REQ_CHANNEL', '-1001774639766'))
+
 import logging
 
 logger = logging.getLogger(__name__)
@@ -164,6 +167,8 @@ async def advantage_spoll_choker(bot, query):
             k = (movie, files, offset, total_results)
             await auto_filter(bot, query, k)
         else:
+            
+ await client.send_message(req_channel,f"-🦋 #REQUESTED_CONTENT 🦋-\n\n📝Content Name :{search}\nRequested By: {message.from_user.first_name}\n USER ID:{message.from_user.id}\n\n🗃",
             k = await query.message.edit('𝚃𝙷𝙸𝚂 𝙼𝙾𝚅𝙸𝙴 I𝚂 𝙽𝙾𝚃 𝚈𝙴𝚃 𝚁𝙴𝙻𝙴𝙰𝚂𝙴𝙳 𝙾𝚁 𝙰𝙳𝙳𝙴𝙳 𝚃𝙾 𝙳𝙰𝚃𝚂𝙱𝙰𝚂𝙴 💌')
             await asyncio.sleep(10)
             await k.delete()
